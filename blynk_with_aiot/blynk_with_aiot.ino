19b94f8c33ae28b7cac70fa5f6458f7bf86935d5
bekirr12 <bekirsamikaratas@gmail.com>
2025-05-30 10:33:00 +0300

    adding blynk example

---
+++ blynk_with_aiot/blynk_with_aiot.ino
@@ -0,0 +1,113 @@
+#include <WiFi.h>
+#include <BlynkSimpleEsp32.h>
+#include <DHT.h>
+#include <FastLED.h>
+
+// blynk definitions
+#define BLYNK_TEMPLATE_ID "TMPL6wMK0Bxoc"
+#define BLYNK_TEMPLATE_NAME "deney1"
+#define BLYNK_AUTH_TOKEN "Q-fQ4WcwiX_S6lNt9Wqv5_SJzg2fSdDK"
+#define BLYNK_PRINT Serial
+
+// DHT11
+#define DHTPIN 8
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+// pins number
+#define LED_PIN 35
+#define FAN_PIN 10
+#define s1pin 37
+
+#define RGB_PIN 15
+#define NUM_LEDS 1
+CRGB leds[NUM_LEDS];
+
+// Wi-Fi credentials
+char ssid[] = "FiberHGW_TPB4AE";
+char pass[] = "bekiradamerkanadam";
+
+BlynkTimer timer;
+
+int fanSpeed = 0;
+int redVal = 0;
+int greenVal = 0;
+int blueVal = 0;
+
+// V0 Led
+BLYNK_WRITE(V0) {
+  int ledState = param.asInt();
+  digitalWrite(LED_PIN, ledState);
+}
+
+// V1 Fan
+BLYNK_WRITE(V1) {
+  int percent = param.asInt(); // 0–100 arası
+  fanSpeed = map(percent, 0, 100, 0, 255); // PWM'e çevir
+  ledcWrite(FAN_PIN, fanSpeed);
+}
+
+// V2 temperature
+void sendTemperature() {
+  float temp = dht.readTemperature();
+  if (!isnan(temp)) {
+    Blynk.virtualWrite(V2, temp);
+    Serial.print("Temperature Value: ");
+    Serial.println(temp);
+  } else {
+    Serial.println("FAIL!");
+  }
+}
+
+// V3 RGB red
+BLYNK_WRITE(V3) {
+  redVal = param.asInt();
+  updateRGB();
+}
+
+// V4 RGB green
+BLYNK_WRITE(V4) {
+  greenVal = param.asInt();
+  updateRGB();
+}
+
+// V5 blue
+BLYNK_WRITE(V5) {
+  blueVal = param.asInt();
+  updateRGB();
+}
+
+void updateRGB() {
+  leds[0] = CRGB(redVal, greenVal, blueVal);
+  FastLED.show();
+
+  Serial.printf("RGB: R=%d, G=%d, B=%d\n", redVal, greenVal, blueVal);
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(s1pin, OUTPUT);
+  digitalWrite(s1pin, HIGH);
+
+  ledcAttach(FAN_PIN, 500, 8);
+
+  // connect blynk
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+  dht.begin();
+
+  FastLED.addLeds<NEOPIXEL, RGB_PIN>(leds, NUM_LEDS);
+  leds[0] = CRGB::Black;
+  FastLED.show();
+
+  // Send data every 1 second
+  timer.setInterval(1000L, sendTemperature);
+}
+
+void loop() {
+  Blynk.run();
+  timer.run();
+}
+
+
